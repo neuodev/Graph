9764cec124a8594535c6f8456c08db825c9d8774
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-12 06:52:00 +0200

    Build the Graph class

---
+++ Graph.ts
@@ -0,0 +1,22 @@
+class Vertex {
+  label: string;
+  map: object;
+  constructor(label) {
+    this.label = label;
+    this.map = {};
+  }
+}
+class Graph {
+  map = {};
+
+  addNode(label: string) {
+    const vertex = new Vertex(label);
+    if (!this.map[label]) this.map[label] = vertex;
+  }
+}
+
+const graph = new Graph();
+graph.addNode('Jone');
+graph.addNode('Doe');
+graph.addNode('Jane');
+console.log(graph);
